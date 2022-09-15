0e12a6961e5491c74050f1bd40191eacd28648ec
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-09-15 12:11:15 +0530

    added menu Item to the Form

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Data.SqlTypes;
+using System.Globalization;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,14 +10,61 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WinForms
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
+namespace WinForms {
+    public partial class Form1 : Form {
+        private MenuStrip menuStrip1 = new MenuStrip ();
+        private ToolStripMenuItem menu1 = new ToolStripMenuItem ();
+        private ToolStripMenuItem fileMenuItem = new ToolStripMenuItem ();
+
+        public Form1 () {
+            InitializeComponent ();
         }
 
+        public void FormLayout () {
+            LoadForm ();
+            LoadMenu ();
+        }
+
+        private void LoadForm () {
+            this.Name = "Telugu Alphabet Examination";
+            this.Text = "Telugu Alphabet Examination";
+            this.BackColor = System.Drawing.Color.White;
+            this.Size = new System.Drawing.Size (500, 500);
+            this.ControlBox = true;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog ;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.KeyPreview = true;
+        }
+
+        private void LoadMenu () {
+            // menuStrip1
+            this.menuStrip1.Items.AddRange (new ToolStripItem[] {
+                menu1
+            });
+            this.menuStrip1.BackColor = System.Drawing.Color.Black;
+            this.menuStrip1.Location = new System.Drawing.Point (0, 0);
+            this.menuStrip1.Size = new System.Drawing.Size (800, 24);
+            this.menuStrip1.Name = "menuStrip1";
+
+            // Menu1
+            this.menu1.Image = System.Drawing.Image.FromFile ("images/menuHamburger.png");
+            this.menu1.Size = new System.Drawing.Size (180, 20);
+            this.menu1.Name = "menu1";
+            this.menu1.AutoSize = false;
+            this.menu1.DropDownItems.AddRange (new ToolStripItem[] {
+                this.fileMenuItem
+            });
+
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.AutoSize = false;
+            this.fileMenuItem.Size = new System.Drawing.Size (180, 20);
+            this.fileMenuItem.Text = "Exit";
+            this.fileMenuItem.Click += new System.EventHandler (this.Mouse_Click);
+            this.Controls.Add (this.menuStrip1);
+        }
+
+        void Mouse_Click (object sender, EventArgs e) {
+            Application.Exit ();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,20 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WinForms
-{
-    static class Program
-    {
+namespace WinForms {
+    static class Program {
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
-        {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+        static void Main () {
+            Form1 form1 = new Form1 ();
+            form1.FormLayout ();
+            Application.Run (form1);
+        }
+
+        static void MainJunk () {
+            Application.SetHighDpiMode (HighDpiMode.SystemAware);
+            Application.EnableVisualStyles ();
+            Application.SetCompatibleTextRenderingDefault (false);
+            Application.Run (new Form1 ());
         }
     }
 }
