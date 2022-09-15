f7896ec783a5daf0450a52f4eab19dccf9e1e2a8
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-09-15 12:30:37 +0530

    added button click events

--- Form1.cs
+++ Form1.cs
@@ -34,10 +34,13 @@ namespace WinForms {
             button1.Text = "Next >>";
             // Set the position of the button on the form.
             button1.Location = new Point (400, 400);
+            button1.Click += new EventHandler (NextButtonClick);
+
             // Set the text of button2 to "Cancel".
             button2.Text = "<< Previous";
             // Set the position of the button based on the location of button1.
             button2.Location = new Point (button1.Left, button1.Height + button1.Top + 10);
+            button2.Click += new EventHandler (PreviousButtonClick);
 
             // Add button1 to the form.
             this.Controls.Add (button1);
@@ -81,6 +84,14 @@ namespace WinForms {
             this.fileMenuItem.Text = "Exit";
             this.fileMenuItem.Click += new System.EventHandler (this.Mouse_Click);
             this.Controls.Add (this.menuStrip1);
+        }
+
+        void NextButtonClick (object sender, EventArgs e) {
+            MessageBox.Show ("Next button clicked", "Button Clicked");
+        }
+
+        void PreviousButtonClick (object sender, EventArgs e) {
+            MessageBox.Show ("Previous button clicked", "Button Clicked");
         }
 
         void Mouse_Click (object sender, EventArgs e) {
